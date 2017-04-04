e7923c986e3c540824846ff32a0aadafbf747063
Tracy <tgolf4fun@gmail.com>
2017-04-03 22:33:24 -0700

    Final, week 6 homework

---
+++ Assignment/starter_code/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  console.log("LETS GO!!");
+  //clear out any classes already there
+  $('body').removeClass();
+  $('#city-type').click(cityBackground);
+    // create an array of cities
+    var myCities = ["NYC","SF","LA","ATX","SYD"];
+    //not needed but just making sure I can read my array
+    console.log(myCities[0]);
+
+    //add array values to the option menu on the html page
+    myCities.forEach(function(item){
+            $('#city-type').append($("<option>"+item+"</option>"));
+        });
+    //Update html page background
+    function cityBackground(){
+        event.preventDefault();
+    // get user input
+    var city = $('#city-type').val();
+    var city = (city.trim());
+    // conditionals for input comparison to defined cities
+    if (city === "NYC") {
+        //use the attr instead of addClass method
+        $('body').attr('class', 'nyc');
+        }
+    else if (city === "SF") {
+        $('body').attr('class', 'sf');
+        }
+    else if (city === "LA") {
+        $('body').attr('class', 'la')
+        }
+    else if (city === "ATX") {
+        $('body').attr('class', 'austin')
+        }
+    else if (city == "SYD") {
+        $('body').attr('class', 'sydney')
+        }
+    }
+});
